4a305b52fa5d5de1ac14459e21601914dc5c4ee9
ChandiraVihan <chandiravihan@gmail.com>
2026-01-24 18:11:26 +0530

    basic-ui

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,61 @@
-﻿using System.Text;
+﻿// using System.Text;
+// using System.Windows;
+// using System.Windows.Controls;
+// using System.Windows.Data;
+// using System.Windows.Documents;
+// using System.Windows.Input;
+// using System.Windows.Media;
+// using System.Windows.Media.Imaging;
+// using System.Windows.Navigation;
+// using System.Windows.Shapes;
+
+// namespace OmniSearchApp;
+
+// /// <summary>
+// /// Interaction logic for MainWindow.xaml
+// /// </summary>
+// public partial class MainWindow : Window
+// {
+//     public MainWindow()
+//     {
+//         InitializeComponent();
+//     }
+// }
+
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace OmniSearchApp;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace OmniSearchApp
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+            // This allows to drag the search bar around with your mouse
+            this.MouseDown += (s, e) => { if (e.LeftButton == MouseButtonState.Pressed) DragMove(); };
+        }
+
+        private void SearchInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string query = SearchInput.Text;
+            // TODO: This trigger the Search Engine later
+        }
+
+        private void SearchInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Logic for when the search bar is clicked
+        }
+
+     protected override void OnKeyDown(KeyEventArgs e)
+{
+    base.OnKeyDown(e);
+    // Use Key.Escape to close when esc pressed
+    if (e.Key == Key.Escape)
+    {
+        Application.Current.Shutdown();
+    }
+}
     }
 }
